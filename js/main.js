f1034fdad0a8f1e23a0f4e0f14a82afc627374ac
hwin016000180169-png <hwin016000180169@gmail.com>
2025-10-30 18:15:04 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// const lenis = new Lenis()
+
+// // 스크롤 이벤트 감지 (선택사항)
+// lenis.on('scroll', (e) => {
+//   // console.log(e) // 디버깅용
+// })
+
+// // 애니메이션 프레임으로 부드럽게 업데이트
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
+// requestAnimationFrame(raf)
